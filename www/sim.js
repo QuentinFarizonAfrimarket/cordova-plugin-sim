57a76b34b945606f97dc2223968da51d212676a9
scwl <soren@ikx.no>
2015-05-28 16:41:11 +0200

    Bugfix

--- www/sim.js
+++ www/sim.js
@@ -11,18 +11,6 @@
 		return cordovaRef.exec(successCallback, errorCallback, 'Sim', 'getAirplaneModeStatus', []);
 	};
 	
-	Sim.prototype.enableAirplaneMode = function (successCallback, errorCallback) {
-		return cordovaRef.exec(successCallback, errorCallback, 'Sim', 'enableAirplaneMode', []);
-	};
-	
-	Sim.prototype.disableAirplaneMode = function (successCallback, errorCallback) {
-		return cordovaRef.exec(successCallback, errorCallback, 'Sim', 'disableAirplaneMode', []);
-	};
-	
-	Sim.prototype.toggleAirplaneMode = function (successCallback, errorCallback) {
-		return cordovaRef.exec(successCallback, errorCallback, 'Sim', 'toggleAirplaneMode', []);
-	};
-	
 	Sim.prototype.getServiceState = function (successCallback, errorCallback) {
 		return cordovaRef.exec(successCallback, errorCallback, 'Sim', 'getServiceState', []);
 	};
